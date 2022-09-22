a1ccc17f479132e0fe3a04e7ac3a14d640279a3f
msalves008 <matheusdesousaalves2018@gmail.com>
2022-09-22 12:30:29 -0300

    chore: enabling next to use experimental Image api

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  experimental: {
+    newNextLinkBehavior: true,
+    images: {
+      allowFutureImage: true,
+    },
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import Image from "next/future/image"
+import { Container, Header } from "../styles/pages/app"
+import logo from '../assets/logo.svg'
+globalStyles()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
