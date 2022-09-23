637dad2d19b71aefbea593f5e61c603e25c174ff
msalves008 <matheusdesousaalves2018@gmail.com>
2022-09-23 12:54:20 -0300

    build: add stripe domain to next future image

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  images: {
+    domains: [
+      'files.stripe.com',
+    ],
+  },
+
   experimental: {
     newNextLinkBehavior: true,
     images: {
